d34d21006f0c44622c4298462bbc3909ee810a5e
Loek <L_V_D_BERG@hotmail.com>
2019-12-18 11:07:16 +0100

    Portfolio commit

---
+++ Loeks FPS/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int scoreToWin;
+    public int curScore;
+
+    public bool gamePaused;
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            TogglePauseGame();
+    }
+
+    public void TogglePauseGame()
+    {
+        gamePaused = !gamePaused;
+        Time.timeScale = gamePaused == true ? 0.0f : 1.0f;
+
+        Cursor.lockState = gamePaused == true ? CursorLockMode.None : CursorLockMode.Locked; 
+
+        //toggle pause menu
+        GameUI.instance.TogglePauseMenu(gamePaused);
+    }
+
+    public void AddScore(int score)
+    {
+        curScore += score;
+
+        GameUI.instance.UpdateScoreText(curScore);
+        if (curScore >= scoreToWin)
+            WinGame();
+    }
+
+    void WinGame()
+    {
+        GameUI.instance.SetEndGameScreen(true, curScore);
+
+        Time.timeScale = 0.0f;
+        gamePaused = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoseGame()
+    {
+        GameUI.instance.SetEndGameScreen(false, curScore);
+
+        Time.timeScale = 0.0f;
+        gamePaused = true;
+        Cursor.lockState = CursorLockMode.None; 
+    }
+}
---
+++ Loeks FPS/Assets/Scripts/PickUp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickupType
+{
+    Health,
+    Ammo
+}
+
+public class PickUp : MonoBehaviour
+{
+    public PickupType type;
+    public int value;
+    public AudioClip pickupSfx;
+
+    [Header("Bobbing")]
+    public float rotateSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3 startPos;
+    private bool bobbingUp;
+
+    
+
+    private void Start()
+    {
+        //set the start position
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        //rotating
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+
+        //bobbing up/down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0, bobHeight / 2,0) : new Vector3(0, -bobHeight,0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if (transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+
+            switch (type)
+            {
+                case PickupType.Health:
+                    player.GiveHealth(value);
+                    break;
+                case PickupType.Ammo:
+                    player.GiveAmmo(value);
+                    break;
+            }
+            other.GetComponent<AudioSource>().PlayOneShot(pickupSfx); 
+            gameObject.SetActive(false);
+        } 
+    }
+}
